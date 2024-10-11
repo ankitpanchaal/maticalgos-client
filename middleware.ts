b6148905a76622153bd074ed4af62c6346308bbd
ankitpanchaal <ankit0panchal@gmail.com>
2024-10-12 01:11:03 +0530

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -2,34 +2,39 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { jwtVerify } from "jose";
 import { cookies } from "next/headers";
-import { API_ENDPOINT } from "./lib/constants/app-constants";
 
 const protectedRoutes = ["/dashboard"];
 
 export default async function middleware(request: NextRequest) {
     const cookieStore = cookies();
-    const _token = cookieStore.get("token");
+    const token = cookieStore.get("token");
     
     const isProtectedRoute = protectedRoutes.some((prefix) =>
-      request.nextUrl.pathname.startsWith(prefix)
-  );
-  
-  if (!_token && isProtectedRoute) {
-    console.log(
-      "Redirecting to signin due to lack of session on protected route"
+        request.nextUrl.pathname.startsWith(prefix)
     );
-    const absoluteURL = new URL(API_ENDPOINT as string);
-    return NextResponse.redirect(absoluteURL.toString());
-  }
-  if(!_token?.value) return NextResponse.redirect(API_ENDPOINT as string);
 
-  try {
-    const { payload } = await jwtVerify(_token.value, new TextEncoder().encode(process.env.JWT_SECRET!));
-    if(!payload?.acname) return NextResponse.redirect(API_ENDPOINT as string);
-  } catch (error) {
-    console.error("JWT verification failed", error);
-    return NextResponse.redirect(API_ENDPOINT as string);
-  }
+    // If it's not a protected route, allow the request to proceed
+    if (!isProtectedRoute) {
+        return NextResponse.next();
+    }
 
-  return NextResponse.next();
+    // For protected routes, check for a valid token
+    if (!token?.value) {
+        console.log("Redirecting to signin due to lack of token on protected route");
+        return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    try {
+        const { payload } = await jwtVerify(token.value, new TextEncoder().encode(process.env.JWT_SECRET!));
+        if (!payload?.acname) {
+            throw new Error("Invalid token payload");
+        }
+    } catch (error) {
+        console.error("JWT verification failed", error);
+        return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    // If we reach here, the token is valid for a protected route
+    return NextResponse.next();
 }
+
